323eec40bdd2c29f82649b972ed00f735188dd9a
”Pavlo <levkopavlo@gmail.com>
2024-07-23 16:52:30 +0300

    List Comprehension

---
+++ str/string.py
@@ -0,0 +1,6 @@
+my_srt = ('The field_name itself begins with an arg_name that is either a '
+          'number or a keyword. If it’s a number, it refers to a positional '
+          'argument, and if it’s a keyword, it refers to a named keyword '
+          'argument. If the numerical arg_names in a format ')
+print(my_srt)
+
