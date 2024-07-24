4aafec55f48dc89aab987ff529497c2c47fc02f9
”Pavlo <levkopavlo@gmail.com>
2024-07-24 21:11:23 +0300

    File Manager

---
+++ FileAssistent/FileManager.py
@@ -0,0 +1,6 @@
+path = '/Users/macbook/PycharmProjects/pythonProject/FileAssistent/text.txt'
+file = open(path, 'r')
+text = file.read()
+file.close()
+print(text)
+
---
+++ FileAssistent/WriteTextInFile.py
@@ -0,0 +1,4 @@
+with open('file.txt', 'w') as file:  # Here you create new file
+    file.write('Hello World')
+
+
